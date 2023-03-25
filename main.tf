dc99bb8891a66ee6801aeeb87798674bccdf35ec
Felix Gündling <46090606+fguendling@users.noreply.github.com>
2023-03-25 12:53:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,12 @@ provider "google" {
   project = "abx50c5xvoxqqnhrwaqfziooysf2or"
 }
 
+provider "kubernetes" {
+  host = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+}
+
 resource "google_container_cluster" "primary" {
   name = "nuwe-web-app"
   location = "us-central1-a"
