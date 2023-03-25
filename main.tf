e759b25c361a95470a851d69eca8d2e0b0e63b65
Felix Gündling <46090606+fguendling@users.noreply.github.com>
2023-03-25 13:11:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,9 +19,9 @@ resource "google_container_cluster" "primary" {
 }
 
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  host                   = "https://${data.google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
 }
 
 resource "kubernetes_deployment" "kd" {
