63005332eccd41257fea4a4b99f886537f0bb6cb
Felix Gündling <46090606+fguendling@users.noreply.github.com>
2023-03-25 12:26:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,62 +11,8 @@ provider "google" {
   project = "abx50c5xvoxqqnhrwaqfziooysf2or"
 }
 
-resource "kubernetes_deployment" "example" {
-  metadata {
-    name = "terraform-example"
-    labels = {
-      test = "MyExampleApp"
-    }
-  }
-
-  spec {
-    replicas = 3
-
-    selector {
-      match_labels = {
-        test = "MyExampleApp"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          test = "MyExampleApp"
-        }
-      }
-
-      spec {
-        container {
-          image = "gcr.io/abx50c5xvoxqqnhrwaqfziooysf2or/mms-cloud-skeleton-fork@sha256:61dc8b6cef2fba42c87ac68ef672758f21aad8efbbb42844d137f50dfd517e72"
-          name  = "example"
-
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-          liveness_probe {
-            http_get {
-              path = "/"
-              port = 80
-
-              http_header {
-                name  = "X-Custom-Header"
-                value = "Awesome"
-              }
-            }
-
-            initial_delay_seconds = 3
-            period_seconds        = 3
-          }
-        }
-      }
-    }
-  }
+resource "google_container_cluster" "primary" {
+  name = "nuwe-web-app"
+  location = "us-central1-a"
+  initial_node_count = 2
 }
